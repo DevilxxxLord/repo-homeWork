69c10d607c89b47de73fde065b9ea73fcf553dcd
devilxxxlord <tian.i@bk.ru>
2021-04-20 11:45:00 +0300

    added homework_5

---
+++ level_5/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Level_5
+{
+    class Employee
+    {
+        public string FIO { get; set; }
+        public string Position { get; set; }
+        public string Email { get; set; }
+        public string Number { get; set; }
+        public int Salary { get; set; }
+        public int Age { get; set; }
+        public Employee(string FIO, string Positon, string Email, string Number, int Salary, int Age)
+        {
+            this.FIO = FIO;
+            this.Position = Position;
+            this.Email = Email;
+            this.Number = Number;
+            this.Salary = Salary;
+            this.Age = Age;
+        }
+        public string Info()
+        {
+            return
+                this.FIO + ", " +
+                this.Position + ". Email: " +
+                this.Email + ". Phone number: " +
+                this.Number + ", " +
+                this.Salary + ", " +
+                this.Age;
+        }
+    }
+}
---
+++ level_5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace Level_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            ReadAddFile();
+            //2
+            AppendFile();
+            //3
+            WriteNumber();
+            //4         
+            Employee[] employees = {
+                new Employee("Ivanov Ivan Ivanovich", "Engineer", "ivan@jmail.com", "89634452345", 40000, 30),
+                new Employee("Ivanov Dmitrii Dmitrievich", "Programmer", "dmitrii@jmail.com", "89688885434", 80000, 41),
+                new Employee("Ivanov Alexander Alexandrovich", "Analyst", "alex@jmail.com", "89634452345", 60000, 40),
+                new Employee("Ivanova Svetlana Mihailovna", "Developer", "sveta@jmail.com", "89634452345", 100000, 27),
+                new Employee("Ivanov Vladimir Vladimirovich", "Seller", "vlad@jmail.com", "89634452345", 200000, 43)
+            };
+            for (int i = 0; i < employees.Length; i++)
+            {
+                if (employees[i].Age < 40)
+                {
+                    Console.WriteLine(employees[i].Info());
+                }
+            }
+
+
+        }
+        static void ReadAddFile()
+        {
+            //1
+            Console.WriteLine("Введите строку, для добавления в файл .txt:");
+            string TextUser = Console.ReadLine();
+            File.WriteAllText("text.txt", TextUser);
+        }
+        static void AppendFile()
+        {
+            //2
+            File.AppendAllText("startup.txt", DateTime.Now.ToLongTimeString() + "\n");
+        }
+        static void WriteNumber()
+        {
+            //3
+            Console.WriteLine("Введите произвольный набор чисел от 0 до 255:");
+            string number = Console.ReadLine();
+            File.WriteAllText("number.bin", number);
+        }
+    }
+
+}
