b2d657be2aaa3174405c7b0d729d0efd63d25d83
devilxxxlord <tian.i@bk.ru>
2021-04-21 17:03:34 +0300

    added homework_6

---
+++ Level_6/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using System.Security.Permissions;
+using System.Security.Principal;
+using System.Diagnostics;
+
+namespace Level_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            ItemProcess();
+            ProcessAnd();
+
+        }
+        static void ItemProcess()
+        {
+            Process[] procs = Process.GetProcesses();
+
+            foreach (Process item in procs)
+            {
+                Console.WriteLine($"Название процесса: {item.ProcessName}, id:{item.Id}");
+            }
+        }
+        static void ProcessAnd()
+        {
+            Console.WriteLine("введите Id процесса для закрытия: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Если хотите убить процесс, нажмите 2, если хотите просто закрыть приложение - 1");
+            int i = Convert.ToInt32(Console.ReadLine());
+            switch (i)
+            {
+                case 1:
+                    Process p = Process.GetProcessById(b);
+                    p.CloseMainWindow();
+                    break;
+                case 2:
+                    Process p2 = Process.GetProcessById(b);
+                    p2.Kill();
+                    break;
+            }               
+        }
+    }
+}
